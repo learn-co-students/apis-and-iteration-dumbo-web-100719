b4a4a40bbdb4f62e1e62e4fd91d26fc4c2b9f67a
github username <github email address>
2019-10-21 16:27:41 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,10 +2,44 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+def welcome 
+  puts "Welcome to the Star Wars API"
+  puts "Which character would you like to search for?"
+  gets.chomp.downcase
+end 
+
+def get_all_characters
+  all_the_characters= Array(1..9).map do |page_num|
+  response_string = RestClient.get("http://www.swapi.co/api/people/?page=#{page_num}")
   response_hash = JSON.parse(response_string)
+  response_hash["results"]
+  end.flatten
+end 
+
+def find_characters_from_api(character_name)
+  characters = get_all_characters
+  characters.find do |character|
+    character["name"].downcase == character_name
+  end
+end 
+
+def take_film_url_and_return_film_objects(film_url)
+  film_url.map do |film_url|
+    response_string = RestClient.get(film_url)
+    response_hash = JSON.parse(response_string)
+  end
+end 
+
+def get_character_movies_from_api(character_name)
+  character_object = find_characters_from_api(character_name)
+  films = character_object["films"]
+  film_objects = take_film_url_and_return_film_objects(films)
+  film_objects.sort_by{|film| film["episode_id"]}
+
+  
+  #make the web request
+  # response_string = RestClient.get('http://www.swapi.co/api/people/')
+  # response_hash = JSON.parse(response_string)
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -20,6 +54,9 @@ end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each do |film|
+    puts film["episode_id"].to_s + ":" +film["title"]
+  end
 end
 
 def show_character_movies(character)
